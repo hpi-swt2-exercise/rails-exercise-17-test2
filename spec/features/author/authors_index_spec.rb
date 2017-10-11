76a77d23a39e6e7ab199bc1c68ee7dac167d6e2d
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 15:27:07 +0200

    Failing test for #11

--- spec/features/author/authors_index_spec.rb
+++ spec/features/author/authors_index_spec.rb
@@ -6,4 +6,9 @@ describe "New author page", type: :feature do
     visit authors_path
   end
 
+  it "should render withour error" do
+    visit authors_path
+
+    expect(page).to have_text('Homepage: '+@author.homepage)
+  end
 end
