13e8e4bf7b0bb568c5b4ccc5cfa4a5f6f01382b7
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 14:44:27 +0200

    Failing test for #6

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -20,4 +20,13 @@ describe "New author page", type: :feature do
     expect(page).to have_css('input[type="submit"]')
   end
 
+
+  it "save a newly created author" do
+    visit new_author_path
+    fill_in "author_first_name", :with => 'Alan'
+    fill_in "author_last_name", :with => 'Turing'
+    fill_in "author_homepage", :with => 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+end
+
 end
