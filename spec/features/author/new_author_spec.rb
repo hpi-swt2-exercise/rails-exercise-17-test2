0b303886ea23b06641e58f58c67db9057429350f
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 11:49:39 +0200

    Failing test for #2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,13 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit new_author_path
   end
+
+  it "should have text input for first name, last name, and homepage" do
+    visit new_author_path
+	  
+    expect(page).to have_field('First name')
+	expect(page).to have_field('Last name')
+	expect(page).to have_field('Homepage')
+  end
+
 end
