010721d537def268f42e9627df013dc3ba923314
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 16:29:43 +0200

    Failing test for #16

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -6,4 +6,13 @@ describe "Author edit page", type: :feature do
     author = FactoryGirl.create :author
     visit edit_author_path(author)
   end
+
+  
+  it "should have text input for first name, last name, and homepage" do
+    visit new_author_path
+    
+    expect(page).to have_field('First name')
+    expect(page).to have_field('Last name')
+    expect(page).to have_field('Homepage')
+  end
 end
