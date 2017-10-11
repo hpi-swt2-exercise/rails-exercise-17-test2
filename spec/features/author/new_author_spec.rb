af6ffd08e93cfa14b076036ea62d8b06326b7e8a
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 11:54:03 +0200

    Failing test for #3

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -14,4 +14,10 @@ describe "New author page", type: :feature do
 	expect(page).to have_field('Homepage')
   end
 
+  it "should have a submitt button" do
+    visit new_author_path
+	  
+    expect(page).to have_css('input[type="submit"]')
+  end
+
 end
