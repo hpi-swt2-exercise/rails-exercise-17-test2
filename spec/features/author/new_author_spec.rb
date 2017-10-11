a52138cfc6cf0edea4f2092372d975cb8f92492e
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 16:05:55 +0200

    Add failting test for #14

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -21,12 +21,23 @@ describe "New author page", type: :feature do
   end
 
 
-  it "save a newly created author" do
+  it "should save a newly created author" do
     visit new_author_path
     fill_in "author_first_name", :with => 'Alan'
     fill_in "author_last_name", :with => 'Turing'
     fill_in "author_homepage", :with => 'http://wikipedia.org/Alan_Turing'
     find('input[type="submit"]').click
-end
+  end
+
+
+  it "should validate new authors" do
+    visit new_author_path
+    fill_in "author_first_name", :with => ''
+    fill_in "author_last_name", :with => ''
+    find('input[type="submit"]').click
+
+    expect(page).to have_text("First name can't be blank")
+    expect(page).to have_text("Last name can't be blank")
+  end
 
 end
