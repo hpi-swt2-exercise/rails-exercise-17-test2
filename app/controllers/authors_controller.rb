c9980dec1f005d76fec3b87ab3d79efb71053448
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 14:45:24 +0200

    Save author. Closes #6

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,17 @@
 class AuthorsController < ApplicationController
 
+	def show
+	    @author = Author.find(params[:id])
+    end
+ 
+
 	def new
+	end
+
+	def create
+	  @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+
+	  @author.save
+	  redirect_to @author
 	end
 end
