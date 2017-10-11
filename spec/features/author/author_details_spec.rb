002e460d0adcdbecc310e96368d2088f75bedc51
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 15:08:50 +0200

    Failing test for #8

--- spec/features/author/author_details_spec.rb
+++ spec/features/author/author_details_spec.rb
@@ -7,8 +7,15 @@ describe "Author details page", type: :feature do
     @author = FactoryGirl.create :author
     visit "/authors/" + @author.id.to_s
 
-    expect(page).to have_text('name')
+    expect(page).to have_text('First name:')
+    expect(page).to have_text('Alan')
   end
 
+  it "should display the last name" do
+    @author = FactoryGirl.create :author
+    visit "/authors/" + @author.id.to_s
 
+    expect(page).to have_text('Last name:')
+    expect(page).to have_text('Turing')
+  end
 end
