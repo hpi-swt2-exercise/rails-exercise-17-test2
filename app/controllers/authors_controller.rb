656aab61e4e2b62d7d2087e088d2c3d19ed812ef
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 16:34:53 +0200

    Closes #16

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,11 +12,21 @@ class AuthorsController < ApplicationController
     	@author = Author.find(params[:id])
 	end
 
+	def update
+	  @author = Author.find(params[:id])
+	 
+	  if @author.update(author_params)
+	    redirect_to @author
+	  else
+	    render 'edit'
+	  end
+	end
+
 	def new
 	end
 
 	def create
-	  @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+	  @author = Author.new(author_params)
 
 	  if @author.save
 	    redirect_to @author
@@ -24,4 +34,10 @@ class AuthorsController < ApplicationController
 	    render 'new'
 	  end
 	end
+
+	private 
+		def author_params
+			params.require(:author).permit(:first_name, :last_name, :homepage)
+		end
+
 end
--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -7,12 +7,25 @@ describe "Author edit page", type: :feature do
     visit edit_author_path(author)
   end
 
-  
+
   it "should have text input for first name, last name, and homepage" do
-    visit new_author_path
+    author = FactoryGirl.create :author
+    visit edit_author_path(author)
     
     expect(page).to have_field('First name')
     expect(page).to have_field('Last name')
     expect(page).to have_field('Homepage')
   end
+
+
+  it "should save the edited author" do
+    author = FactoryGirl.create :author
+    visit edit_author_path(author)
+    
+    fill_in "author_first_name", :with => 'Alan2'
+    fill_in "author_last_name", :with => 'Turing2'
+    fill_in "author_homepage", :with => 'http://wikipedia.org/Alan_Turing2'
+    find('input[type="submit"]').click
+  end
+
 end
