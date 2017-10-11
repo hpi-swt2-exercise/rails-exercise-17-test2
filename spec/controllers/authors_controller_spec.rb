24db6ea21f0f39fe25d5af824a90b8c1400d9481
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-11 17:03:00 +0200

    Failing test for #18

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
 
+  it "responds to DELETE" do
+  	@author = FactoryGirl.create :author
+  	delete :destroy, :id => @author.id
+  end
+
 end
