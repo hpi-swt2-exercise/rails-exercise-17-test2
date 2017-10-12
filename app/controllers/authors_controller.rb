81b43c7fb2071c038ecdfcdb8638f0c646e8aeb6
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-12 09:40:50 +0200

    Fix delete. Closes #19

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -13,9 +13,10 @@ class AuthorsController < ApplicationController
 	end
 
 	def destroy
-    	@author = Author.find(params[:id])
-	    @author.destroy
-   		redirect_to author_path(@author)
+	  @author = Author.find(params[:id])
+	  @author.destroy
+	 
+	  redirect_to authors_path
   	end
 
 	def update
