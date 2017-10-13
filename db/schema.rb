9da29633b46d26e19ea8deaf1bdccb5a02633a3b
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-13 13:02:45 +0200

    Fix schema. Close #33

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,16 @@ ActiveRecord::Schema.define(version: 20171013105509) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "first_name"
+    t.integer  "paper_id"
     t.string   "last_name"
     t.string   "homepage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "papers", ["authors_id"], name: "index_papers_on_authors_id"
+  add_index "authors", ["paper_id"], name: "index_authors_on_paper_id"
 
   create_table "papers", force: :cascade do |t|
-    t.integer  "authors_id"
     t.string   "title"
     t.string   "venue"
     t.integer  "year"
