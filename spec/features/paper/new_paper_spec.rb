96071c424bf1704e9899cfc5c15dbbb1afabc117
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-13 12:05:18 +0200

    Add failing test for #23

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -13,11 +13,29 @@ describe "New paper page", type: :feature do
 	  expect(page).to have_field('Venue')
 	  expect(page).to have_field('Year')
   end
-  
+
   it "should have a submitt button" do
-    visit new_author_path
+    visit new_paper_path
     
     expect(page).to have_css('input[type="submit"]')
+  end
+
+  it "should save a newly created paper" do
+    visit new_paper_path
+    fill_in "paper_title", :with => 'Title'
+    fill_in "paper_venue", :with => 'Venue'
+    fill_in "paper_year", :with => '2017'
+    find('input[type="submit"]').click
+  end
+
+  it "should validate new papers" do
+    visit new_paper_path
+
+    fill_in "paper_title", :with => ''
+    fill_in "paper_venue", :with => 'Venue'
+    fill_in "paper_year", :with => '2017'
+    find('input[type="submit"]').click
+    expect(page).to have_text("Title can't be blank")
   end
 
 
