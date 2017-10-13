b2afd49a4f79428bd0217ba857bd5f62ed13b69e
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-13 12:05:27 +0200

    Add validations. Closes #23

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -2,4 +2,24 @@ class PapersController < ApplicationController
 
 	def new
 	end
+
+	def show
+	    @paper = Paper.find(params[:id])
+    end
+
+	def create
+	  @paper = Paper.new(paper_params)
+
+	  if @paper.save
+	    redirect_to @paper
+	  else
+	    render 'new'
+	  end
+	end
+
+	private 
+		def paper_params
+			params.require(:paper).permit(:title, :venue, :year)
+		end
+
 end
