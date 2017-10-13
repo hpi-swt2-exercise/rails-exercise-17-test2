28a6370ddb2fc0b7f154dd0ef2c1b4344fb6bc3d
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-13 12:42:33 +0200

    Add delete action for papers. Closes #31

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -35,6 +35,13 @@ class PapersController < ApplicationController
 	  end
 	end
 
+	def destroy
+	  @paper = Paper.find(params[:id])
+	  @paper.destroy
+	 
+	  redirect_to papers_path
+  	end
+
 	private 
 		def paper_params
 			params.require(:paper).permit(:title, :venue, :year)
