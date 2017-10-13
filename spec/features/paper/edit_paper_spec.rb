c61205bf2dab34a12e96505305dd0ac7bdcd6071
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-10-13 12:35:43 +0200

    Failing test for #30

---
+++ spec/features/paper/edit_paper_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "Paper edit page", type: :feature do
+  
+  it "should render withour error" do
+    paper = FactoryGirl.create :paper
+    visit edit_paper_path(paper)
+  end
+
+
+  it "should have text input for title, venue, and year" do
+    paper = FactoryGirl.create :paper
+    visit edit_paper_path(paper)
+    
+    expect(page).to have_field('Title')
+    expect(page).to have_field('Venue')
+    expect(page).to have_field('Year')
+  end
+
+
+  it "should save the edited paper" do
+    paper = FactoryGirl.create :paper
+    visit edit_paper_path(paper)
+    
+    fill_in "paper_title", :with => 'Title'
+    fill_in "paper_venue", :with => 'Venue'
+    fill_in "paper_year", :with => '2017'
+    find('input[type="submit"]').click
+  end
+
+end
